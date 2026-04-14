bcd8e1abca70bc89bd8b98c1dc27b32d4671378e
ShadoeBoris <boris@jocoy.org>
2026-04-13 20:53:45 -0700

    Post quemu emulation bound to Docker

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudshell.dev','https://*.github.dev','http://localhost:8000','http://localhost:8080']
